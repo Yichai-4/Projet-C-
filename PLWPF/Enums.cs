760bd5e3bea69ae53f5cc2cdbd99fd1878622b23
hazanyichai <hazanyichai@gmail.com>
2020-01-23 17:47:10 +0200

    Enums

---
+++ PLWPF/Enums.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLWPF
+{
+    class Enums
+    {
+        public enum HostingUnit
+        {
+            Add,
+            Update,
+            Delete
+        }
+
+        public enum Pool
+        {
+            Necessary,
+            Possible,
+            Uninterested
+        }
+
+        public enum Area
+        {
+            All,
+            North,
+            South,
+            Center,
+            Jerusalem
+        }
+
+        public enum Status
+        {
+            Active,
+            Unactive
+        }
+
+        public enum Type
+        {
+            Zimmer,
+            Hotel,
+            Camping,
+            Villa
+        }
+
+        public enum Order
+        {
+            Add,
+            Update
+        }
+    }
+}
--- PLWPF/GuestRequest.xaml.cs
+++ PLWPF/GuestRequest.xaml.cs
@@ -15,13 +15,29 @@ using System.Windows.Shapes;
 namespace PLWPF
 {
     /// <summary>
-    /// Logique d'interaction pour GuestRequest.xaml
+    /// Logique d'interaction pour AddGuestRequest.xaml
     /// </summary>
     public partial class GuestRequest : Window
     {
         public GuestRequest()
         {
             InitializeComponent();
+            cbPool.ItemsSource = Enum.GetValues(typeof(Enums.Pool));
+            cbJacuzzi.ItemsSource = Enum.GetValues(typeof(Enums.Pool));
+            cbGarden.ItemsSource = Enum.GetValues(typeof(Enums.Pool));
+            cbAttractions.ItemsSource = Enum.GetValues(typeof(Enums.Pool));
+            cbArea.ItemsSource = Enum.GetValues(typeof(Enums.Area));
+            //cbStatus.ItemsSource = Enum.GetValues(typeof(Enums.Status));
+            cbType.ItemsSource = Enum.GetValues(typeof(Enums.Type));
         }
+
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(tbAdults.Text) < 0 || int.Parse(tbChildren.Text) < 0)
+                MessageBox.Show("Number of adults can't be negative", "ERROR", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            //this.Close();
+        }
+
+
     }
 }
